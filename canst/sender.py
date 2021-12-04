1f9ea38237ffe5564167a8a220d27d2722004b7c
xia0long <xiaolong.zn@gmail.com>
2021-12-04 23:43:48 +0800

    update sender

--- canst/sender.py
+++ canst/sender.py
@@ -10,25 +10,28 @@ def get_frame_from_file(
     file_path: str = None,
     start_line: int = None,
     end_line: int = None,
-) -> Frame:
-
+) -> list:
+    
+    frame_list = []
     with open(file_path, "r") as f:
-        current_line_index = 0
+        index = 0
         for line in f.readlines():
-            if not line:
-                continue
-
+            
             # Skip the lines before the start line
-            if start_line and current_line_index < start_line:
-                current_line_index += 1
+            if start_line is not None and index < start_line:
+                index += 1
                 continue
 
             # Skip the lines after the end line
-            if end_line and current_line_index > end_line:
+            elif end_line is not None and index > end_line:
                 break
 
+            else:
+                index += 1
             frame = str_to_frame(line.split(" ")[-1])
-            yield frame
+            frame_list.append(frame)
+    
+    return frame_list
 
 
 def send(
@@ -38,7 +41,6 @@ def send(
     file_path: str = None,
     start_line: int = None,
     end_line: int = None,
-    arb_id_filter: str = None,
     loop: bool = False,
 ) -> None:
     def _send(message, delay):
@@ -54,19 +56,16 @@ def send(
             for message, delay in zip(messages, delays):
                 _send_process = Process(target=_send, args=[message, delay])
                 _send_process.start()
-                _send_process.join()
 
         elif messages and not delays:
             for message in messages:
                 dev.send(str_to_frame(message))
 
         elif file_path:
-
             frames = get_frame_from_file(
                 file_path,
                 start_line=start_line,
                 end_line=end_line,
-                arb_id_filter=arb_id_filter,
             )
             while True:
                 for frame in frames:
@@ -78,5 +77,4 @@ def send(
             print("Wrong args.")
 
     except KeyboardInterrupt:
-        _send_process.terminate()
         print("\nSend canceled.")
